feae705e1ef7db211fbe5688b816ed9d9bc42b8f
boi10ty <phaxoay1@gmail.com>
2026-05-21 20:53:19 +0700

    Update layout.tsx

--- src/app/Ads-Policy-review/layout.tsx
+++ src/app/Ads-Policy-review/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: 'Page Ads-Policy Review',
+    title: '',
 };
 
 export default function AccountQualityStatusLayout({
